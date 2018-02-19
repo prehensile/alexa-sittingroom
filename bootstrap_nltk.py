afc6398bc399eddf4c93bf594f3efb341189afe0
prehensile <me@prehensile.co.uk>
2018-02-19 22:38:17 +0000

    made bootstrap script better

--- bootstrap_nltk.py
+++ bootstrap_nltk.py
@@ -1,3 +1,10 @@
+import os
 import nltk
 
-nltk.download('perluniprops')
+pth = os.path.realpath( os.path.dirname(__file__) )    
+pth = os.path.join( pth, "src/nltk_data" )
+    
+if not os.path.exists( pth ):
+    os.makedirs( pth )
+
+nltk.download( 'perluniprops', download_dir=pth )
